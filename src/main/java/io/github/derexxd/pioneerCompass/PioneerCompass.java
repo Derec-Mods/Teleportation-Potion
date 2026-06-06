15fd7ed053f30624cda0ab88d4ffe3ac116ea51c
Derek <derex1987@gmail.com>
2026-06-05 22:06:26 -0500

    feat: startup logs

--- src/main/java/io/github/derexxd/pioneerCompass/PioneerCompass.java
+++ src/main/java/io/github/derexxd/pioneerCompass/PioneerCompass.java
@@ -1,5 +1,7 @@
 package io.github.derexxd.pioneerCompass;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class PioneerCompass extends JavaPlugin {
@@ -7,6 +9,12 @@ public final class PioneerCompass extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
+        Bukkit.getLogger().info("");
+        Bukkit.getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "  |_______|                             " +
+                "  ");
+        Bukkit.getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "  | Derex |     Pioneer Compass v" + getDescription().getVersion());
+        Bukkit.getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "  |_______|     Running on " + Bukkit.getName() + " - " + Bukkit.getVersion());
+        Bukkit.getLogger().info("");
 
     }
 
