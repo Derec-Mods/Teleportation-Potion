f42b80d446f675b7621951fe2ae2f04c86ef8b88
Derek <derex1987@gmail.com>
2026-06-05 22:01:13 -0500

    feat: new

---
+++ src/main/java/io/github/derexxd/pioneerCompass/PioneerCompass.java
@@ -0,0 +1,17 @@
+package io.github.derexxd.pioneerCompass;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PioneerCompass extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
