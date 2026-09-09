38370eff7e6871d17e972d71d039749cf8cf11d1
Derek <derex1987@gmail.com>
2026-09-09 01:25:11 -0500

    feat: you can now set the number of bottles a player gets to begin with

--- src/main/java/io/github/derexxd/teleportationPotion/TeleportationPotion.java
+++ src/main/java/io/github/derexxd/teleportationPotion/TeleportationPotion.java
@@ -17,11 +17,14 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class TeleportationPotion extends JavaPlugin {
 
+    public static int STARTER_BOTTLES;
+
     private LocationPreloader preloader;
 
     @Override
     public void onEnable() {
         saveDefaultConfig();
+        STARTER_BOTTLES = Math.max(0, getConfig().getInt("potion.starter-bottles", 3));
 
         LocationPool pool = new LocationPool();
         SafetyChecker safetyChecker = new SafetyChecker(this);
--- src/main/java/io/github/derexxd/teleportationPotion/potion/PotionJoinListener.java
+++ src/main/java/io/github/derexxd/teleportationPotion/potion/PotionJoinListener.java
@@ -1,9 +1,11 @@
 package io.github.derexxd.teleportationPotion.potion;
 
+import io.github.derexxd.teleportationPotion.TeleportationPotion;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.inventory.ItemStack;
 
 public final class PotionJoinListener implements Listener {
 
@@ -19,6 +21,11 @@ public final class PotionJoinListener implements Listener {
         if (player.hasPlayedBefore()) {
             return;
         }
-        player.getInventory().addItem(potionItem.create());
+        if (TeleportationPotion.STARTER_BOTTLES <= 0) {
+            return;
+        }
+        ItemStack bottles = potionItem.create();
+        bottles.setAmount(TeleportationPotion.STARTER_BOTTLES);
+        player.getInventory().addItem(bottles);
     }
 }
